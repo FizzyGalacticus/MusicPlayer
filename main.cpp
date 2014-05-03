be8a4caff994f8dc0ab4f4e271f6130a3e5c21e5
Dustin L. Dodson <dustinleedodson@gmail.com>
2014-05-03 14:06:25 -0800

    Made mainWindow un-resizable, and moved buttons to the bottom of the window.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QLayout>
+#include <QHBoxLayout>
+#include <QtGui>
+#include <QWidget>
+#include <QMainWindow>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-   /* w.setMinimumHeight(2*72);
+
+    w.setMinimumHeight(2*72);
     w.setMinimumWidth(4*72);
     w.setMaximumHeight(2*72);
-    w.setMaximumWidth(4*72);*/
+    w.setMaximumWidth(4*72);
+
     w.show();
 
     return a.exec();
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,21 +19,21 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(&prevButton, SIGNAL(clicked()), this, SLOT(prevButtonIsPressed()));
     prevButton.setParent(this);
     prevButton.setIcon(prevButtonIcon);
-    prevButton.setGeometry(0,0,90,50);
+    prevButton.setGeometry(0,(2*72)-50,(4*72/3),50);
     prevButton.show();
 
     /*************SETTING UP PLAY BUTTON******************/
     connect(&playButton, SIGNAL(clicked()), this, SLOT(playButtonIsPressed()));
     playButton.setParent(this);
     playButton.setIcon(playButtonPlayIcon);
-    playButton.setGeometry(prevButton.width(),0,prevButton.width(),prevButton.height());
+    playButton.setGeometry(prevButton.width(),(2*72)-50,prevButton.width(),prevButton.height());
     playButton.show();
 
     /*************SETTING UP NEXT BUTTON******************/
     connect(&nextButton, SIGNAL(clicked()), this, SLOT(nextButtonIsPressed()));
     nextButton.setParent(this);
     nextButton.setIcon(nextButtonIcon);
-    nextButton.setGeometry(playButton.width()+prevButton.width(),0,prevButton.width(),prevButton.height());
+    nextButton.setGeometry(playButton.width()+prevButton.width(),(2*72)-50,prevButton.width(),prevButton.height());
     nextButton.show();
 }
 
