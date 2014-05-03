b28750f9fe642afe772602532fbdaa8025f9d61e
Dustin L. Dodson <dustinleedodson@gmail.com>
2014-05-03 13:43:30 -0800

    Moved mainWindow icon to icons folder.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QLayout>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+   /* w.setMinimumHeight(2*72);
+    w.setMinimumWidth(4*72);
+    w.setMaximumHeight(2*72);
+    w.setMaximumWidth(4*72);*/
+    w.setLayout(new QLayout);
     w.show();
 
     return a.exec();
