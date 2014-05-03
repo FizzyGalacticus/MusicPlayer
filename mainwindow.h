ba9c0bdfec94d2bb7f013a57d49ac3e26b0c99a4
Matthew Parrish <mcparrish3@alaska.edu>
2014-05-03 15:03:12 -0800

    moar cleaning(mainwindow.h)

--- mainwindow.h
+++ mainwindow.h
@@ -50,26 +50,9 @@ private slots:
     }
 
 
-    // ////////////////////////
-    //void newFile();
+
     void open();
     void play(); // ////////
-    //void pause();
-    //void save();
-    //void print();
-    //void undo();
-    //void redo();
-   // void cut();
-   // void copy();
-    //void paste();
-    /*void bold();
-    void italic();
-    void leftAlign();
-    void rightAlign();
-    void justify();
-    void center();
-    void setLineSpacing();
-    void setParagraphSpacing();*/
     void about();
     void aboutQt();
     // //////////////////////
@@ -98,29 +81,15 @@ private:
 
     QMenu *fileMenu;
     QMenu *playMenu;
-   // QMenu *formatMenu;
+
     QMenu *helpMenu;
     QActionGroup *alignmentGroup;
-    //QAction *newAct;
+
     QAction *openAct;
     QAction *playAct; // ////////////
-    //QAction *pauseAct;
-    //QAction *saveAct;
-    //QAction *printAct;
+
     QAction *exitAct;
-   // QAction *undoAct;
-    //QAction *redoAct;
-    //QAction *cutAct;
-    //QAction *copyAct;
-   // QAction *pasteAct;
-    /*QAction *boldAct;
-    QAction *italicAct;
-    QAction *leftAlignAct;
-    QAction *rightAlignAct;
-    QAction *justifyAct;
-    QAction *centerAct;
-    QAction *setLineSpacingAct;
-    QAction *setParagraphSpacingAct;*/
+
     QAction *aboutAct;
     QAction *aboutQtAct;
     QLabel *infoLabel;
