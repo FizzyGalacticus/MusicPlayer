df116a1bc4626c48a4c8f52d4df3fe7b09bef834
Nathan Helms <natehelms_8@gmx.com>
2014-05-04 17:48:40 -0800

    Took out hard path so no more changing
    We have to select our songs now before we play

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -131,7 +131,7 @@ MainWindow::MainWindow(QWidget *parent) :
 
     /******************SOUND CODE******************/
     _player = new QMediaPlayer;
-    _player->setMedia(QUrl::fromLocalFile("/home/dustin/CS/MusicPlayer/cs372-FinalProject/test.mp3"));
+   // _player->setMedia(QUrl::fromLocalFile());
     _player->setVolume(100);
 
 }
