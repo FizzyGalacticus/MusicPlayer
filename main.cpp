ceab658b3ae9089046e985fc8fab6c1a8b010c5c
Dustin L. Dodson <dustinleedodson@gmail.com>
2014-05-09 11:30:55 -0800

    Added Fusion style to application.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setStyle(QStyleFactory::create("Fusion"));
+
+    for(int i = 0; i < QStyleFactory::keys().size(); i++)
+    {
+        qDebug() << QStyleFactory::keys()[i];
+    }
+
     MainWindow w;
 
     w.show();
