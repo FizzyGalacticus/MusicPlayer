a0b226e5b4d439002326b98e42f9840e5141c777
Dustin L. Dodson <dustinleedodson@gmail.com>
2014-05-11 02:12:02 -0800

    Got rid of debug printing in main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setStyle(QStyleFactory::create("Fusion"));
 
-    for(int i = 0; i < QStyleFactory::keys().size(); i++)
-    {
-        qDebug() << QStyleFactory::keys()[i];
-    }
-
     MainWindow w;
 
     w.show();
