db23c12c789ee070f94a98e4eb43f984c70cd868
Dustin L. Dodson <dustinleedodson@gmail.com>
2015-05-07 06:23:11 -0800

    Synced worbench model with my actual MySQL schema that was functional.

--- resources/schema.sql
+++ resources/schema.sql
@@ -3,7 +3,7 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
 DROP SCHEMA IF EXISTS `Media_Player` ;
-CREATE SCHEMA IF NOT EXISTS `Media_Player` ;
+CREATE SCHEMA IF NOT EXISTS `Media_Player` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
 USE `Media_Player` ;
 
 -- -----------------------------------------------------
@@ -13,10 +13,13 @@ DROP TABLE IF EXISTS `Media_Player`.`Artist` ;
 
 CREATE  TABLE IF NOT EXISTS `Media_Player`.`Artist` (
   `name` VARCHAR(45) NOT NULL ,
-  `id` INT NOT NULL AUTO_INCREMENT ,
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `name_UNIQUE` (`name` ASC) )
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = latin1
+COLLATE = latin1_swedish_ci;
 
 
 -- -----------------------------------------------------
@@ -26,18 +29,22 @@ DROP TABLE IF EXISTS `Media_Player`.`Song` ;
 
 CREATE  TABLE IF NOT EXISTS `Media_Player`.`Song` (
   `Title` VARCHAR(100) NOT NULL ,
-  `lyrics` LONGTEXT NULL ,
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `filePath` TEXT NULL ,
-  `Artist_id` INT NOT NULL ,
+  `lyrics` LONGTEXT NULL DEFAULT NULL ,
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `filePath` TEXT NULL DEFAULT NULL ,
+  `Artist_id` INT(11) NOT NULL ,
   PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `subkey_UNIQUE` (`Title` ASC, `Artist_id` ASC) ,
   INDEX `fk_Song_Artist1` (`Artist_id` ASC) ,
   CONSTRAINT `fk_Song_Artist1`
     FOREIGN KEY (`Artist_id` )
     REFERENCES `Media_Player`.`Artist` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 872
+DEFAULT CHARACTER SET = latin1
+COLLATE = latin1_swedish_ci;
 
 
 -- -----------------------------------------------------
@@ -47,11 +54,22 @@ DROP TABLE IF EXISTS `Media_Player`.`Album` ;
 
 CREATE  TABLE IF NOT EXISTS `Media_Player`.`Album` (
   `Title` VARCHAR(45) NOT NULL ,
-  `AlbumCover` LONGBLOB NULL ,
-  `id` INT NOT NULL AUTO_INCREMENT ,
+  `AlbumCover` LONGBLOB NULL DEFAULT NULL ,
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `Artist_id` INT(11) NOT NULL ,
   PRIMARY KEY (`id`) ,
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) )
-ENGINE = InnoDB;
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
+  UNIQUE INDEX `subkey_UNIQUE` (`Title` ASC, `Artist_id` ASC) ,
+  INDEX `fk_Album_Artist1` (`Artist_id` ASC) ,
+  CONSTRAINT `fk_Album_Artist1`
+    FOREIGN KEY (`Artist_id` )
+    REFERENCES `Media_Player`.`Artist` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 886
+DEFAULT CHARACTER SET = latin1
+COLLATE = latin1_swedish_ci;
 
 
 -- -----------------------------------------------------
@@ -61,9 +79,11 @@ DROP TABLE IF EXISTS `Media_Player`.`Playlist` ;
 
 CREATE  TABLE IF NOT EXISTS `Media_Player`.`Playlist` (
   `Name` VARCHAR(45) NOT NULL ,
-  `id` INT NOT NULL AUTO_INCREMENT ,
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
   PRIMARY KEY (`id`) )
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1
+COLLATE = latin1_swedish_ci;
 
 
 -- -----------------------------------------------------
@@ -73,7 +93,7 @@ DROP TABLE IF EXISTS `Media_Player`.`Song_has_Playlist` ;
 
 CREATE  TABLE IF NOT EXISTS `Media_Player`.`Song_has_Playlist` (
   `Song_id` INT(11) NOT NULL ,
-  `Playlist_id` INT NOT NULL ,
+  `Playlist_id` INT(11) NOT NULL ,
   PRIMARY KEY (`Song_id`, `Playlist_id`) ,
   INDEX `fk_Song_has_Playlist_Playlist1` (`Playlist_id` ASC) ,
   INDEX `fk_Song_has_Playlist_Song1` (`Song_id` ASC) ,
@@ -83,35 +103,13 @@ CREATE  TABLE IF NOT EXISTS `Media_Player`.`Song_has_Playlist` (
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_Song_has_Playlist_Playlist1`
-    FOREIGN KEY (`Playlist_id`)
-    REFERENCES `Media_Player`.`Playlist` (`id`)
+    FOREIGN KEY (`Playlist_id` )
+    REFERENCES `Media_Player`.`Playlist` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `Media_Player`.`Album_has_Artist`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `Media_Player`.`Album_has_Artist` ;
-
-CREATE  TABLE IF NOT EXISTS `Media_Player`.`Album_has_Artist` (
-  `Album_id` INT NOT NULL ,
-  `Artist_id` INT NOT NULL ,
-  PRIMARY KEY (`Album_id`, `Artist_id`) ,
-  INDEX `fk_Album_has_Artist_Artist1` (`Artist_id` ASC) ,
-  INDEX `fk_Album_has_Artist_Album1` (`Album_id` ASC) ,
-  CONSTRAINT `fk_Album_has_Artist_Album1`
-    FOREIGN KEY (`Album_id` )
-    REFERENCES `Media_Player`.`Album` (`id` )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_Album_has_Artist_Artist1`
-    FOREIGN KEY (`Artist_id` )
-    REFERENCES `Media_Player`.`Artist` (`id` )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1
+COLLATE = latin1_swedish_ci;
 
 
 -- -----------------------------------------------------
@@ -120,8 +118,8 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `Media_Player`.`Album_has_Song` ;
 
 CREATE  TABLE IF NOT EXISTS `Media_Player`.`Album_has_Song` (
-  `Album_id` INT NOT NULL ,
-  `Song_id` INT NOT NULL ,
+  `Album_id` INT(11) NOT NULL ,
+  `Song_id` INT(11) NOT NULL ,
   PRIMARY KEY (`Album_id`, `Song_id`) ,
   INDEX `fk_Album_has_Song_Song1` (`Song_id` ASC) ,
   INDEX `fk_Album_has_Song_Album1` (`Album_id` ASC) ,
@@ -135,7 +133,9 @@ CREATE  TABLE IF NOT EXISTS `Media_Player`.`Album_has_Song` (
     REFERENCES `Media_Player`.`Song` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1
+COLLATE = latin1_swedish_ci;
 
 
 -- -----------------------------------------------------
@@ -144,16 +144,19 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `Media_Player`.`User` ;
 
 CREATE  TABLE IF NOT EXISTS `Media_Player`.`User` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `username` VARCHAR(45) NOT NULL ,
   `password` BLOB NOT NULL ,
-  `Fname` VARCHAR(20) NULL ,
-  `Lname` VARCHAR(20) NULL ,
-  `email` VARCHAR(100) NULL ,
+  `Fname` VARCHAR(20) NULL DEFAULT NULL ,
+  `Lname` VARCHAR(20) NULL DEFAULT NULL ,
+  `email` VARCHAR(100) NULL DEFAULT NULL ,
   `joinDateTime` DATETIME NOT NULL ,
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC) ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB;
+  PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) )
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = latin1
+COLLATE = latin1_swedish_ci;
 
 
 -- -----------------------------------------------------
@@ -162,9 +165,9 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `Media_Player`.`User_has_Song` ;
 
 CREATE  TABLE IF NOT EXISTS `Media_Player`.`User_has_Song` (
-  `User_id` INT NOT NULL ,
-  `Song_id` INT NOT NULL ,
-  `numberOfListens` INT NOT NULL DEFAULT 0 ,
+  `User_id` INT(11) NOT NULL ,
+  `Song_id` INT(11) NOT NULL ,
+  `numberOfListens` INT(11) NOT NULL DEFAULT '0' ,
   PRIMARY KEY (`User_id`, `Song_id`) ,
   INDEX `fk_User_has_Song_Song1` (`Song_id` ASC) ,
   INDEX `fk_User_has_Song_User1` (`User_id` ASC) ,
@@ -178,7 +181,9 @@ CREATE  TABLE IF NOT EXISTS `Media_Player`.`User_has_Song` (
     REFERENCES `Media_Player`.`Song` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1
+COLLATE = latin1_swedish_ci;
 
 
 
